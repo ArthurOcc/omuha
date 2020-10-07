eda8c759c5d392e65224740d71c273b28e3d9f37
ArthurOcc <arthur.aocc@gmail.com>
2020-10-07 20:17:25 -0300

    Add files via upload

---
+++ scripts/player.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+var nome = 'player'
+var life = 0
+var _class = 'druid'
+var currentLocation = Vector2(0,0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
